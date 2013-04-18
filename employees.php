344c5510f57e0daef2402f349992147775fd2770
Matthew Hansen <MHansen@Matthews-MacBook-Pro-2.local>
2013-04-18 09:41:58 -0700

    set plugin up
    added discriptors to main plugin files

---
+++ employees.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Employees
+Plugin URI: http://flightofthedodo.com
+Description: Custom post type for custom author pages.
+Version: 1.0.0 alpha
+Author: Matthew R. Hansen
+Author URI: http://flightofthedodo.com
+License: GPLv2
+*/
+?>
+
+add
