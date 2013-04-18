ed364f09a2b7cbcf6f086435dc0a57712cae0b12
Matthew Hansen <MHansen@Matthews-MacBook-Pro-2.local>
2013-04-18 10:49:50 -0700

    finished custom post type
    finsihed setting up custom posttype

--- employees.php
+++ employees.php
@@ -8,6 +8,35 @@ Author: Matthew R. Hansen
 Author URI: http://flightofthedodo.com
 License: GPLv2
 */
-?>
 
-add
+add_action( 'init', 'create_employees'); 
+
+function create_employees() {
+	register_post_type( 'employees', 
+		array(
+			'labels' => array(
+				'name' => 'Employee\'s', 
+				'singular_name' => 'Employee\'s',
+				'add_new' => 'Add New', 
+				'add_new_item' => 'Add New Employee',
+				'edit' => 'Edit', 
+				'edit_item' => 'Edit Employee', 
+				'new_item' => 'New Employee',
+				'view' => 'View', 
+				'view_item' => 'View Employee',
+				'search_items' => 'Search Employee\'s', 
+				'not_found' => 'No Employee\'s Found',
+				'not_found_in_trash' => 'No Employee\'s found in Trash', 
+				'parent' => 'Parent Employee'
+			),
+			'public' => true,
+			'menu_position' => 15,
+			'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'), 
+			'taxonomies' => array(''),
+			'menu_icon' => plugins_url('images/image.png', __FILE__ ),
+			'has_archive' => true
+		)
+	);
+}
+
+?>
